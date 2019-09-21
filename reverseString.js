3665f4b0b14a4a540fc0c26fc4ff55c732c496b1
Paul Wu <paulwujr@gmail.com>
2019-09-21 12:42:58 -0400

    developed reverseString function, split() reverse() join()

--- reverseString.js
+++ reverseString.js
@@ -1,5 +1,7 @@
-let reverseString = function (userInput) {
-
-
-    
-}
+var reverseString = function(userInput) {
+    var splitInput = userInput.split("");
+    var reverseInput = splitInput.reverse();
+    var joinInput = reverseInput.join("");
+    return joinInput;
+  }
+  
