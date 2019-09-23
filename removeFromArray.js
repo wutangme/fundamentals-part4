7af81c54669faee9a1621f1ef56974eaabb89b8b
Paul Wu <paulwujr@gmail.com>
2019-09-23 19:52:56 -0400

    removeFromArray funcntion

--- removeFromArray.js
+++ removeFromArray.js
@@ -1,5 +1,11 @@
-funtion removeFromArray() {
-
-
-    
-}
+const removeFromArray = function(...args) {
+    const array = args[0];
+    const newArray = [];
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+          newArray.push(item);
+        } 
+    });
+    return newArray;
+  };
+  
