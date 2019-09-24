74cb47b13b8668e25b73ec98f85b24da2993664a
Paul Wu <paulwujr@gmail.com>
2019-09-23 20:04:49 -0400

    sumALl function

--- sumAll.js
+++ sumAll.js
@@ -1,8 +1,21 @@
-function sumAll (...) {
+const sumAll = function(startNum, endNum) {
 
-
-
-
-
+    if (startNum < 0 || endNum < 0) 
+      return "Please choose positive values";
+    if (typeof startNum !== "number" || typeof endNum !== "number") 
+      return "Please choose a number";
+  
+    if (startNum > endNum) {
+      const holdStartNum = startNum;
+      startNum = endNum;
+      endNum = holdStartNum;
+    }
+  
+    let summedNumbers = 0;
+  
+    for (let i = startNum; i < endNum + 1; i++) {
+      summedNumbers += i;
+    }
     
-}
+    return summedNumbers;
+  
